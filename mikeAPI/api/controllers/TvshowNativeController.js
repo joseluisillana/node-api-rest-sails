df430cafac80897f97f91789f40d745036253adc
joseluis.illana@beeva.com <joseluis.illana@beeva.com>
2015-12-03 14:48:59 +0100

    Rama para experimentar con el driver nativo de MongoDB para Node.js

--- mikeAPI/api/controllers/TvshowNativeController.js
+++ mikeAPI/api/controllers/TvshowNativeController.js
@@ -2,21 +2,7 @@
 * Mongo Native service
 */
 var mongoNativeService = require('../services/mongoNativeService.js');
-
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('tvshowmongooses');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
-    callback(result);
-  });
-}
+var test_data = require('../exampleData/test-data');
 
 module.exports = {
   // (Mirar si mola esto) Solo cargamos el cliente una vez y lo compartimos,
@@ -24,9 +10,43 @@ module.exports = {
   //mongoNativeService.MongoClient
   create: function(req,res){
     console.log('M.I.K.E - POST ' + req.url);
-    insertDocuments(mongoNativeService.mongoNativeDB, function() {
-      mongoNativeDB.close();
+    var teamlist = test_data.teamlist;
+    console.log('M.I.K.E - team list ' + JSON.stringify(teamlist));
+    mongoNativeService.teamlist("D", function(err, teamlist) {
+      console.log('M.I.K.E - FLAG mongoNativeService.teamlist...');
+      if (!err) {
+        console.log('M.I.K.E - FLAG EL IF ERR...');
+        var strTeam = "",
+          i = 0;
+          console.log('M.I.K.E - teamlist.count...'+ teamlist.count);
+          console.log('M.I.K.E - teamlist.teams...'+ teamlist.teams);
+          console.log('M.I.K.E - teamlist.teams.length...'+ teamlist.teams.length);
+        for (i = 0; i < teamlist.count;) {
+          for (z = 0; z < teamlist[i].teams.count;) {
+            strTeam = strTeam + "<li>" + teamlist[i].teams[z].country + "</li>";
+            console.log('M.I.K.E - teamlist.teams[i].country...' + teamlist[i].teams[z].country);
+            z = z + 1;
+          }
+          i = i + 1;
+        }
+        strTeam = "<ul>" + strTeam + "</ul>";
+        /*res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        res.write(template.build("Test web page on node.js", "Hello there", "<p>The teams in Group " + teamlist.GroupName + " for Euro 2012 are:</p>" + strTeam));
+        res.end();
+        */
+        res.status(200).send("The teams in Group " + teamlist.GroupName + " for Euro 2012 are: " + strTeam);
+      } else {
+          console.log('M.I.K.E - FLAG EL ELSE...');
+        /*res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });*/
+        /*res.write(template.build("Oh dear", "Database error", "<p>Error details: " + err + "</p>"));
+        res.end();*/
+        res.status(500).send("Oh dear, Database error Error details: " + JSON.stringify(err) + err);
+      }
     });
-    res.status(200).send("Psss a saber");
+    //res.status(200).send("Psss a saber");
   }
 }
--- mikeAPI/api/services/mongoNativeService.js
+++ mikeAPI/api/services/mongoNativeService.js
@@ -1,39 +1,84 @@
 /*
   Cargamos el cliente de MongoDB Nativo
 */
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert'), mongoNativeDB;
 
 // URL
-var url = 'mongodb://10.211.55.112:27017/restexample';
+var urlLong = 'mongodb://10.211.55.112:27017/restexample';
+var host = '10.211.55.112';
+var port = 27017;
+var database = 'restexample';
 
-/*
-Usamos el método connect para abrir una conexión con el servidor
+var collectionName = 'teams';
 
-Nos servimos de los métodos de testeo para comprobar la conexión
-*/
-/*MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("[ MONGO NATIVE DRIVER ] - Connected correctly to server");
-  mongoNativeDB = db;
-  //db.close();
-});*/
+var mongo = require('mongodb'),
+  Server = mongo.Server,
+  Db = mongo.Db;
+
+var server = new Server(host, port, {
+  auto_reconnect: true
+});
+
+var db = new Db(database, server);
+
+var onErr = function(err, callback) {
+  console.log('M.I.K.E - [[ERROR]] : '+JSON.stringify(err));
+  db.close();
+  callback(err);
+};
+
 
 /**
  * Let's make our Mongodb Schemas/Models
  */
  module.exports = {
-   MongoClient: MongoClient,
-   function connectToMongo(url, callback) {
-     if (mongoNativeDB) { callback(null, mongoNativeDB);
-     }else
-     {
-       MongoClient.connect(url, function(err, conn) {
-       assert.equal(null, err);
-       console.log("[ MONGO NATIVE DRIVER ] - Connected correctly to server");
-       mongoNativeDB = conn;
-       callback(err, conn);
-     });
-   }
+   teamlist : function(gname, callback) {
+    db.open(function(err, db) {
+      if (!err) {
+        db.collection('teams', function(err, collection) {
+          console.log('M.I.K.E - VAMOSSSSS... 1 ' + collection);
+          if (!err) {
+            console.log('M.I.K.E - VAMOSSSSS... 2 ' + collection);
+            collection.find({
+              'GroupName': gname
+            }).toArray(function(err, docs) {
+              if (!err) {
+                console.log('M.I.K.E - [[llego?]] : ');
+                db.close();
+                var intCount = docs.length;
+                if (intCount > 0) {
+                  var strJson = "";
+                  for (var i = 0; i < intCount;) {
+                    var intTeamsCount = docs[i].teams.length;
+                    for (var z = 0; z < intTeamsCount;) {
+                      strJson += '{"country":"' + docs[i].teams[z].country + '"}'
+                      z = z + 1;
+                      if (z < intTeamsCount) {
+                        strJson += ',';
+                      }
+                    }
+                    i = i + 1;
+                    if (i < intCount) {
+                      strJson += ',';
+                    }
+                  }
+                  strJson = '{"GroupName":"' + gname + '","count":' + intCount + ',"teams":[' + strJson + "]}"
+                  console.log('M.I.K.E - [[strJson]] : ' + strJson);
+                  callback("", JSON.parse(strJson));
+                }
+              } else {
+                console.log('M.I.K.E - [[ERROR]] : '+err);
+                onErr(err, callback);
+              }
+            }); //end collection.find
+          } else {
+            console.log('M.I.K.E - [[ERROR]] : '+err);
+            onErr(err, callback);
+          }
+        }); //end db.collection
+      } else {
+        console.log('M.I.K.E - [[ERROR]] : '+err);
+        onErr(err, callback);
+      }
+    });
   }
  }
